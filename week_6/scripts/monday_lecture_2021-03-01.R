e10bc65d71f6b9b7dd5752475ba4d3a101b5aefc
RuthVR <ruth.vergarareyes.450@my.csun.edu>
2021-03-04 00:35:52 -0800

    This weeks work is in the week_6 file

---
+++ week_6/scripts/monday_lecture_2021-03-01.R
@@ -0,0 +1,9 @@
+### Today we will be  learning and practicing how to use RMarkdown. ###
+### Created by: Ruth Vergara Reyes ###
+### Updated on: 2021-03-01 ###
+
+#########################################################################
+
+### Load Libraries ###
+library(rmarkdown)
+library(tinytex)                       ### This saves markdown as a PDF ###
